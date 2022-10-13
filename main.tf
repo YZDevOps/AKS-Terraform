862616fe89d5d50ec9bf30974c2594eb5e1e847a
YZDevOps <114833920+YZDevOps@users.noreply.github.com>
2022-10-13 22:19:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,20 @@ resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group
   location = var.azure_region
 }
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
 
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  location            = var.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
   location            = azurerm_resource_group.aks_rg.location
