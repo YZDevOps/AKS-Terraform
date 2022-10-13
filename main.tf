fba9386318399ae0b38a32419c450ff805be26b9
YZDevOps <114833920+YZDevOps@users.noreply.github.com>
2022-10-13 23:08:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_resource_group" "aks_rg" {
 resource "azurerm_role_assignment" "role_acrpull" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  principal_id                     = data.azuread_service_principal.aks_principal.id
   skip_service_principal_aad_check = true
 }
 
