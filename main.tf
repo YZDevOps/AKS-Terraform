4ec6913ae1733f8598129ff3e4562c662307a290
YZDevOps <114833920+YZDevOps@users.noreply.github.com>
2022-10-13 22:50:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_role_assignment" "role_acrpull" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = var.acrname
   resource_group_name = azurerm_resource_group.aks_rg.name
   location            = azurerm_resource_group.aks_rg.location
   sku                 = "Standard"
